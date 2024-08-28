a1eb519d6626af6c8bd2b982fcb40b35b2c90da6
Thuzar <user@Thuzar.local>
2024-08-28 13:51:29 +0630

    First Commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("Shaung Lwei Lwei Shaung Lwi..");
+console.log("Shwei Lwei Lwei");
+//untracked files
+// tracked files -> git add file-name
+//commit ->git commit
+//push -> git push origin branch
+//git push origin main
