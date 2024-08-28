a552f8ea826e2f3c5532f8bb5f276f13525ba998
Thuzar <user@Thuzar.local>
2024-08-28 14:08:37 +0630

    Second Commit

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 console.log("Hello World");
 console.log("Shaung Lwei Lwei Shaung Lwi..");
 console.log("Shwei Lwei Lwei");
+console.log("Fuck , I forgot some code. HEHE")
 //untracked files
 // tracked files -> git add file-name
 //commit ->git commit
